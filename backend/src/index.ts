415a2431f59e78bdff5f370a943f26991996de4a
Farhan Malek <malekf2328@gmail.com>
2023-11-23 13:17:31 +1300

    try delpy w bodyparser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import router from './routes/cardetection';
 import createCarRouter from './routes/createcar';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 require('dotenv').config();
 const app:Express = express();
 const url:string = process.env.MONGO_KEY as string;
@@ -10,8 +11,8 @@ const url:string = process.env.MONGO_KEY as string;
 
 //middleware
 app.use(cors());
-app.use(express.json());
-
+// app.use(express.json());
+app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
 //Routes
 app.use("/", router);
 app.use("/", createCarRouter);
