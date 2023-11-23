1afe0769a4851841c18117c3f23ee313a110f7b7
Farhan Malek <malekf2328@gmail.com>
2023-11-23 13:19:22 +1300

    added back in express.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import router from './routes/cardetection';
 import createCarRouter from './routes/createcar';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
 require('dotenv').config();
 const app:Express = express();
 const url:string = process.env.MONGO_KEY as string;
@@ -11,8 +10,8 @@ const url:string = process.env.MONGO_KEY as string;
 
 //middleware
 app.use(cors());
-// app.use(express.json());
-app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
 //Routes
 app.use("/", router);
 app.use("/", createCarRouter);
